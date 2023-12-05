4448c5e0874f2a183a89cc7566360549d3445e3b
Andreu Laguarda <andreulaguarda@gmail.com>
2023-12-05 15:01:04 +0100

    Initial commit

---
+++ src/main/java/com/andreulaguarda/SpringThymeleafDemoProject/SpringThymeleafDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.andreulaguarda.SpringThymeleafDemoProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymeleafDemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringThymeleafDemoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andreulaguarda/SpringThymeleafDemoProject/SpringThymeleafDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreulaguarda.SpringThymeleafDemoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymeleafDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
